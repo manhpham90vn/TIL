bcbdea10f56be8cc1941c4d3765946ee2582fa21
Manh Pham Van (Opn VN - Dev Manager) <manhpham90vn@icloud.com>
2023-11-09 13:00:05 +0700

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -1,5 +1,3 @@
-sudo apt update -y
-sudo apt upgrade -y
 sudo apt install -y \
  curl \
  software-properties-common \
@@ -8,22 +6,25 @@ sudo apt install -y \
  git \
  wget \
  gnupg \
- zsh
+ zsh \
+ vim
 
 # chrome
-wget -q https://dl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
-sudo apt update
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update -y
 sudo apt install -y google-chrome-stable
 
 # keepassxc
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt update
-sudo apt install keepassxc
+sudo add-apt-repository ppa:phoerious/keepassxc -y
+sudo apt update -y
+sudo apt install -y keepassxc
 
 # code
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update -y
 sudo apt install -y code
 
 # docker
@@ -34,11 +35,12 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # shell
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
