4f607df81a2d453bcc3500d9e8880ee9a2899c2c
Manh Pham <manhpham90vn@icloud.com>
2023-11-09 13:24:26 +0700

    update

--- bash/install.sh
+++ bash/install.sh
@@ -10,24 +10,29 @@ sudo apt install -y \
  vim
 
 # chrome
+# https://www.ubuntuupdates.org/ppa/google_chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update -y
 sudo apt install -y google-chrome-stable
 
 # keepassxc
+# https://launchpad.net/~phoerious/+archive/ubuntu/keepassxc
 sudo add-apt-repository ppa:phoerious/keepassxc -y
 sudo apt update -y
 sudo apt install -y keepassxc
 
 # code
+# https://www.ubuntuupdates.org/ppa/vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install apt-transport-https
 sudo apt update -y
 sudo apt install -y code
 
 # docker
+# https://docs.docker.com/engine/install/ubuntu/
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -39,8 +44,43 @@ sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # shell
+# https://ohmyz.sh/#install
 sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+# go
+curl -sSL https://git.io/g-install | sh -s
+
+# python
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+sudo apt update
+sudo apt install -y build-essential \
+libssl-dev \
+zlib1g-dev \
+libbz2-dev \
+libreadline-dev \
+libsqlite3-dev curl \
+libncursesw5-dev \
+xz-utils \
+tk-dev \
+libxml2-dev \
+libxmlsec1-dev \
+libffi-dev \
+liblzma-dev
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# aws
+pyenv install 3.9
+pyenv global 3.9
+python -m pip install awscli awsebcli
+cd /tmp
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip ./aws-sam-cli-linux-x86_64.zip -d sam-cli
+cd sam-cli
+sudo ./install
